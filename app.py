cd9e9be269e7cf51ac3f80b45fc16dee14a4c5fb
RjVictorBaba <your-rjvictorbabaofficiall@gmail.com>
2025-03-29 23:08:52 +0530

    Initial commit

--- app.py
+++ app.py
@@ -36,8 +36,8 @@ def signup():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        username = request.form.get('username')
+        password = request.form.get('password')
         user = User.query.filter_by(username=username).first()
         if user and bcrypt.check_password_hash(user.password, password):
             session['user_id'] = user.id
